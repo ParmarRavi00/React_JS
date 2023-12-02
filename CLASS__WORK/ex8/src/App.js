ddc566afd7a36e28315e6dade2d057a3c3728b15
Parmar Raviraj Rajendrasinh <parmarraviraj00@gmail.com>
2023-12-02 16:24:15 +0530

    .

--- CLASS__WORK/ex8/src/App.js
+++ CLASS__WORK/ex8/src/App.js
@@ -1,64 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
-import CreateConnection from './CreateConnection';
+import { useState } from 'react';
 
-
-
-
-function ChatRoom({roomId})
+function Form()
 {
+  const [name,setName] = useState('Shubham');
 
-  const [serverUrl,setServerUrl] = useState('http:/localhost/4564')
-
- useEffect(()=>{
-
-  const connection = CreateConnection(roomId,serverUrl);
-  connection.connect();
-
-  // return ()=>{
-  //   connection.disconnect();
-  // }
-
- },[serverUrl,roomId]);
-
-  return(
-    <>
-      <input value=  {serverUrl} 
-      onChange={()=>{setServerUrl()}}
-      />
-    </>
-  )
+  return(<>
   
+    <input 
+    value={name}
+    onChange={(e)=>{setName(e.target.value)}}
+   
+    />
+    <h1>{name}</h1>
+
+  </>)
 }
 
 function App() {
 
-  const [roomId,setRoomId] = useState("general");
-  const [show,setShow] = useState(false);
+  const[version,setVersion] = useState(0);
+
+  function handleReset()
+  {
+    setVersion(version+1)
+  }
+
   return (
     <div className="App">
-     
 
-     Choose Chat Room :
-
-    <select 
-    value={roomId}
-    onChange ={(e)=>{setRoomId(e.target.value)}}
-    >
-
-      <option>general</option>
-      <option>music</option>
-      <option>travel</option>
-    </select>
-
-    <button onClick={()=>{setShow(false)}}>
-    {show ? "Close Chat" : "Open Chat"}
-
-    </button>
-
-    <ChatRoom roomId={roomId}/>
-
+      <button onClick={handleReset}>RESET</button>
+     <Form key={version}/>
     </div>
   );
 }
