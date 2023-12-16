9c5d355ab4030e49ef802de3e6f8ca28102fa638
Parmar Raviraj Rajendrasinh <parmarraviraj00@gmail.com>
2023-12-16 17:31:00 +0530

    .

--- CLASS__WORK/redux_crud/src/App.js
+++ CLASS__WORK/redux_crud/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Home from './Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CreateUser from './CreateUser';
@@ -9,12 +9,12 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/create' element={<CreateUser/>}></Route>
-        <Route path='/edit/:userid' element={<EditUser/>}></Route>
-      </Routes>
-      
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/create' element={<CreateUser />}></Route>
+          <Route path='/edit/:userid' element={<EditUser />}></Route>
+        </Routes>
+
       </BrowserRouter>
     </div>
   );
